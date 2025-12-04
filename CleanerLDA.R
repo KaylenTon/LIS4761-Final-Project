3cd7f25a6e6d07328210d6df396db7bc9a170283
Kaylen Ton <kaylent2k@gmail.com>
2025-12-04 13:50:02 -0500

    2021

--- CleanerLDA.R
+++ CleanerLDA.R
@@ -11,7 +11,6 @@ more_stop_words <- tribble(
   ~word, ~lexicon,
   "covid19", "CUSTOM",
   "coronavirus", "CUSTOM",
-  "virus", "CUSTOM",
   "rt", "CUSTOM",
   "covid", "CUSTOM",
   "19", "CUSTOM",
@@ -20,7 +19,13 @@ more_stop_words <- tribble(
   "http", "CUSTOM",
   "covidnineteen", "CUSTOM",
   "don’t", "CUSTOM",
-  "it’s", "CUSTOM"
+  "it’s", "CUSTOM",
+  "hundreds", "CUSTOM",
+  "thousands", "CUSTOM",
+  "millions", "CUSTOM",
+  "thousandth", "CUSTOM",
+  "fauci", "CUSTOM",
+  "amp", "CUSTOM"
 )
 
 full_stop_words <- stop_words %>% 
@@ -34,7 +39,7 @@ full_stop_words <- stop_words %>%
     filter(str_detect(place, "Florida|FL")) %>% 
     select(id, original_text)
   
-  # cleaning original tweets FROM SCRATCH
+  # cleaning 2020 original tweets FROM SCRATCH
   clean2020 <- original_2020 %>% 
     mutate(original_text = as.character(original_text)) %>% 
     mutate(original_text = replace_contraction(original_text)) %>% 
@@ -74,7 +79,11 @@ full_stop_words <- stop_words %>%
                          "respons" = "response",
                          "raise" = "fundraise",
                          "busin" = "business",
-                         "hospita" = "hospital")) %>% 
+                         "hospita" = "hospital",
+                         "vaccin" = "vaccine",
+                         "accounc" = "announce",
+                         "countie" = "county",
+                         "populati" = "population")) %>% 
     count(id, word) %>% 
     filter(n > 10)
   
@@ -82,37 +91,109 @@ full_stop_words <- stop_words %>%
   dtm2020 <- words_2020 %>% 
     cast_dtm(id, word, n)
   
-  lda2020_k15 = LDA(
+  lda2020_k12 = LDA(
     dtm2020,
     k = 12, 
     method = "Gibbs",
     control = list(seed = 67)
   )
   
-  glimpse(lda2020_k15)
+  glimpse(lda2020_k12)
   
-  lda2020_k15 <- lda2020_k15 %>% 
+  lda2020_k12 <- lda2020_k12 %>% 
     tidy(matrix = "beta")
   
-  lda2020_k15 <- lda2020_k15 %>% 
+  lda2020_k12 <- lda2020_k12 %>% 
     arrange(desc(beta)) %>% 
     group_by(topic) %>%
     slice_max(beta, n = 8) %>% 
     mutate(term2 = fct_reorder(term, beta))
   
-  ggplot(lda2020_k15, aes(term2, beta, fill = as.factor(topic))) + 
-    geom_col(show.legend = F) + scale_y_continuous(breaks = seq(0, 1, by = 0.05)) + 
+  ggplot(lda2020_k12, aes(term2, beta, fill = as.factor(topic))) + 
+    geom_col(show.legend = F) +
     facet_wrap(~topic, scales = "free") + 
     coord_flip()
 
 # April to June of 2021 ---------------------------------------------------
 
-data2021 <- read.csv("COVIDTweetsAprilToJune2021.csv")
-
-original_2021 <- data2021 %>% 
-  filter(str_detect(place, "Florida|FL")) %>% 
-  select(id, original_text)
-
-cleaned_2021 <- data2021 %>% 
-  filter(str_detect(place, "Florida|FL")) %>% 
-  select(id, clean_tweet)
+  data2021 <- read.csv("COVIDTweetsAprilToJune2021.csv")
+  
+  original_2021 <- data2021 %>% 
+    filter(str_detect(place, "Florida|FL")) %>% 
+    select(id, original_text)
+  
+  # cleaning 2021 original tweets FROM SCRATCH
+  clean2021 <- original_2021 %>% 
+    mutate(original_text = as.character(original_text)) %>% 
+    mutate(original_text = replace_contraction(original_text)) %>% 
+    mutate(original_text = replace_number(original_text)) %>% 
+    mutate(original_text = str_remove_all(original_text, "https?://.*$")) %>% 
+    mutate(original_text = str_remove_all(original_text, "RT ")) %>% 
+    mutate(original_text = str_remove_all(original_text, "@\\w+")) %>% 
+    mutate(original_text = removePunctuation(original_text)) %>%
+    mutate(original_text = str_remove_all(original_text, "'")) %>% 
+    mutate(original_text = removeNumbers(original_text)) %>% 
+    unnest_tokens(word, original_text) %>% 
+    anti_join(full_stop_words)
+  
+  dictionary_2021 <- original_2021 %>% 
+    select(original_text) %>% 
+    unnest_tokens(word, original_text) %>% 
+    anti_join(full_stop_words) %>%
+    distinct(word) %>% 
+    pull(word)
+  
+  stem2021 <- clean2021 %>% 
+    mutate(word = wordStem(word)) %>% 
+    mutate(
+      word = unlist(lapply(word, stemCompletion, dictionary = dictionary_2021))
+    )
+  
+  words_2021 <- stem2021 %>% 
+    drop_na() %>% 
+    anti_join(full_stop_words) %>% 
+    mutate(word = recode(word,
+                         "viru" = "virus",
+                         "informat" = "information",
+                         "posit" = "positive",
+                         "mani" = "humanity",
+                         "commun" = "community",
+                         "preside" = "president", 
+                         "respons" = "response",
+                         "raise" = "fundraise",
+                         "busin" = "business",
+                         "hospita" = "hospital",
+                         "vaccin" = "vaccine",
+                         "accounc" = "announce",
+                         "countie" = "county",
+                         "populati" = "population")) %>% 
+    count(id, word) %>% 
+    filter(n > 2)
+  
+  # LDA Modeling
+  dtm2021 <- words_2021 %>% 
+    cast_dtm(id, word, n)
+  
+  lda2021_k12 = LDA(
+    dtm2021,
+    k = 12, 
+    method = "Gibbs",
+    control = list(seed = 67)
+  )
+  
+  glimpse(lda2021_k12)
+  
+  lda2021_k12 <- lda2021_k12 %>% 
+    tidy(matrix = "beta")
+  
+  lda2021_k12 <- lda2021_k12 %>% 
+    arrange(desc(beta)) %>% 
+    group_by(topic) %>%
+    filter(beta >= .0125) %>% 
+    slice_max(beta, n = 8) %>% 
+    mutate(term2 = fct_reorder(term, beta))
+  
+  ggplot(lda2021_k12, aes(term2, beta, fill = as.factor(topic))) + 
+    geom_col(show.legend = F) +
+    facet_wrap(~topic, scales = "free") + 
+    coord_flip()
